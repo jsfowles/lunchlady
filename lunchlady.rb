d446b4d766728a52663acdd9506cefaa517d2ff0
Jacob Fowles <js.fowles@gmail.com>
2016-05-14 00:39:48 -0600

    working sub menu selection

--- lunchlady.rb
+++ lunchlady.rb
@@ -1,4 +1,4 @@
-require "pry"
+
 #prompt user for selection of main dishes
 #allow user to select a main dishes
 #allow user to add 2 side dishes
@@ -33,12 +33,44 @@ main = Menu.new("Main Menu", @main_dish)
 side = Menu.new("Side Menu", @side_dish)
 desert = Menu.new("Desert Menu", @desert_menu)
 
-puts "***  Welcome to Juan a Burrito  ***"
-puts "   May I take your order please?   "
-main.display
-side.display
-desert.display
-binding.pry
+@item_total = []
+
+def welcome_menu
+  puts "***  Welcome to Juan a Burrito  ***"
+  puts "What menu would you like to see"
+  puts " 1) Entrees"
+  puts " 2) Sides"
+  puts " 3) Deserts"
+  welcome_input = gets.strip.to_i
+
+   case welcome_input
+   when 1
+     welcome_main = Menu.new("Main Menu", @main_dish)
+     puts `clear`
+     welcome_main.display
+     main_input = gets.strip.to_i
+      if main_input == 1
+        puts @main_dish[0]
+      
+      end
+   when 2
+     welcome_side = Menu.new("Side Menu", @side_dish)
+     puts `clear`
+     welcome_side.display
+   when 3
+     welcome_deseret = Menu.new("Side Menu", @desert_menu)
+     puts `clear`
+     welcome_deseret.display
+   end
+ end
+
+ def main_input
+ end
+ welcome_menu
+ # main.display
+ # side.display
+ # desert.display
+
 
 
 
